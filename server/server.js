6c4290a140d2f17529e72404dfeeb8e164866b6a
GerardoDev25 <GerardoDev25@gmail.com>
2021-09-09 15:57:02 -0400

    this isn't the end

--- server/server.js
+++ server/server.js
@@ -12,18 +12,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+server.listen(port, (err) => {
+	if (err) throw new Error(err);
+
+	console.log(`Servidor corriendo en puerto ${port}`);
+});
+
 // IO = esta es la comunicacion del backend
 module.exports.io = socketIO(server);
 require('./sockets/socket');
-
-
-
-
-
-server.listen(port, (err) => {
-
-    if (err) throw new Error(err);
-
-    console.log(`Servidor corriendo en puerto ${ port }`);
-
-});
