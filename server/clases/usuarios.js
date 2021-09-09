a6c4793296a4234dc685d8dfc079fe9c046c7781
GerardoDev25 <GerardoDev25@gmail.com>
2021-09-09 18:45:17 -0400

    3. Clase para controlar los usuarios del chat

---
+++ server/clases/usuarios.js
@@ -0,0 +1,39 @@
+class Usuario {
+  constructor() {
+    this.personas = [];
+  }
+  // ?
+  agragarPersona(id, nombre) {
+    let persona = { id, nombre };
+    this.personas.push(persona);
+
+    return this.personas;
+  }
+
+  // ?
+  getPersona(id) {
+    return this.personas.filter((pers) => pers.id === id)[0];
+  }
+
+  // ?
+  getPersonas() {
+    return this.personas;
+  }
+
+  // ?
+  getPersonasPorSalas() {
+    // todo
+  }
+
+  // ?
+  borrarPersonas(id) {
+    let personaBorrada = this.getPersona(id);
+    this.personas = this.personas.filter((persona) => persona.id != id);
+
+    return personaBorrada;
+  }
+}
+
+module.exports = {
+  Usuario,
+};
